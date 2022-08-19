6aa7916d9dc6078781414998fed00a3ebaef6e42
JFrederick25 <joey.frederick25@yahoo.com>
2022-08-18 19:04:36 -0500

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GameOptionsComponent } from './components/game-options/game-options.component';
+import { MaterialsComponent } from './components/materials/materials.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, GameOptionsComponent ],
+  declarations: [ AppComponent, GameOptionsComponent, MaterialsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/components/materials/materials.component.ts
+++ src/app/components/materials/materials.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-materials',
+  selector: 'materials',
   templateUrl: './materials.component.html',
   styleUrls: ['./materials.component.css']
 })
