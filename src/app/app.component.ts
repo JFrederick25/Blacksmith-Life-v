b784f73a2f90a78353043c16d3c2bf9663c995fc
JFrederick25 <joey.frederick25@yahoo.com>
2022-08-23 18:03:09 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { PlayerData } from './models/playerData';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  selectedTab: string = 'Materials';
+  selectedTab: string = 'Trading';
   playerData: PlayerData = new PlayerData();
 
   constructor() {
