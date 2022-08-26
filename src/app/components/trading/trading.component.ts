c3190f076408c4cbf477408b9a447f92f786433a
JFrederick25 <joey.frederick25@yahoo.com>
2022-08-26 12:33:58 -0500

    1 file modified

--- src/app/components/trading/trading.component.ts
+++ src/app/components/trading/trading.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { FinishedItem } from '../../models/finishedItem';
-import { Material } from '../../models/material';
 import { PlayerData } from '../../models/playerData';
 import { Vendor } from '../../models/vendor';
-import { lookupMaterialValue } from '../utility/Lookup';
+import { lookupMaterialValue } from '../utility/lookup';
 
-class Entry {name: string; count: number; value: number; type: string}
+class Entry {
+  name: string;
+  count: number;
+  value: number;
+  type: string;
+}
 
 @Component({
   selector: 'trading',
@@ -16,7 +20,7 @@ export class TradingComponent {
   @Input() playerData: PlayerData;
 
   get disabled(): boolean {
-    return this.sellList.length === 0 && this.buyList.length === 0; 
+    return this.sellList.length === 0 && this.buyList.length === 0;
   }
 
   selectedLocation: string = null;
@@ -26,7 +30,7 @@ export class TradingComponent {
 
   get totalSellCost(): number {
     let totalCost = 0;
-    for(const entry of this.sellList) {
+    for (const entry of this.sellList) {
       totalCost += entry.count * entry.value;
     }
     return totalCost;
@@ -34,7 +38,7 @@ export class TradingComponent {
 
   get totalBuyCost(): number {
     let totalCost = 0;
-    for(const entry of this.buyList) {
+    for (const entry of this.buyList) {
       totalCost += entry.count * entry.value;
     }
     return totalCost;
@@ -121,10 +125,15 @@ export class TradingComponent {
   }
 
   sellItemToVendor(item: FinishedItem) {
-    const sellItem = this.sellList.find(sl => sl.name === item.name);
+    const sellItem = this.sellList.find((sl) => sl.name === item.name);
 
     if (!sellItem) {
-      this.sellList.push({name: item.name, count: 1, value: item.value, type: 'item'});
+      this.sellList.push({
+        name: item.name,
+        count: 1,
+        value: item.value,
+        type: 'item',
+      });
     }
   }
 
@@ -134,9 +143,14 @@ export class TradingComponent {
       return;
     }
 
-    const sellMat = this.sellList.find(l => l.name === material);
+    const sellMat = this.sellList.find((l) => l.name === material);
     if (!sellMat) {
-      this.sellList.push({name: material, count: 1, value: lookupMaterialValue(material), type: 'material'});
+      this.sellList.push({
+        name: material,
+        count: 1,
+        value: lookupMaterialValue(material),
+        type: 'material',
+      });
     } else {
       if (pmQty - sellMat.count > 0) {
         sellMat.count++;
@@ -151,9 +165,14 @@ export class TradingComponent {
         return;
       }
 
-      const buyMat = this.buyList.find(l => l.name === material);
+      const buyMat = this.buyList.find((l) => l.name === material);
       if (!buyMat) {
-        this.buyList.push({name: material, count: 1, value: this.selectedVendor.material_cost.get(material), type: 'material'});
+        this.buyList.push({
+          name: material,
+          count: 1,
+          value: this.selectedVendor.material_cost.get(material),
+          type: 'material',
+        });
       } else {
         if (vQty - buyMat.count > 0) {
           buyMat.count++;
@@ -163,10 +182,15 @@ export class TradingComponent {
   }
 
   buyShapeFromVendor(shape: string) {
-    const buyShape = this.buyList.find(bl => bl.name === shape);
+    const buyShape = this.buyList.find((bl) => bl.name === shape);
 
     if (!buyShape) {
-      this.buyList.push({name: shape, count: 1, value: this.selectedVendor.shape_cost.get(shape), type: 'shape'});
+      this.buyList.push({
+        name: shape,
+        count: 1,
+        value: this.selectedVendor.shape_cost.get(shape),
+        type: 'shape',
+      });
     }
   }
 
@@ -175,12 +199,17 @@ export class TradingComponent {
       return;
     }
 
-    for(const entry of this.sellList) {
+    for (const entry of this.sellList) {
       if (entry.type === 'material') {
         const pQty = this.playerData.knownMaterialQuantity.get(entry.name);
-        this.playerData.knownMaterialQuantity.set(entry.name, pQty - entry.count);
+        this.playerData.knownMaterialQuantity.set(
+          entry.name,
+          pQty - entry.count
+        );
 
-        const vMat = this.selectedVendor.materials_List.find(ml => ml === entry.name);
+        const vMat = this.selectedVendor.materials_List.find(
+          (ml) => ml === entry.name
+        );
         if (!vMat) {
           this.selectedVendor.materials_List.push(entry.name);
           this.selectedVendor.material_count.set(entry.name, entry.count);
@@ -192,22 +221,26 @@ export class TradingComponent {
         // add value to player money
       }
       if (entry.type === 'item') {
-        const fItem = this.playerData.finishedItems.find(fi => fi.name === entry.name);
+        const fItem = this.playerData.finishedItems.find(
+          (fi) => fi.name === entry.name
+        );
         const index = this.playerData.finishedItems.indexOf(fItem);
         this.playerData.finishedItems.splice(index, 1);
         // add value to player money
       }
     }
 
-    for(const entry of this.buyList) {
+    for (const entry of this.buyList) {
       if (entry.type === 'material') {
         const vQty = this.selectedVendor.material_count.get(entry.name);
         this.selectedVendor.material_count.set(entry.name, vQty - entry.count);
 
-        const pMat = this.playerData.knownMaterials.find(m => m === entry.name);
+        const pMat = this.playerData.knownMaterials.find(
+          (m) => m === entry.name
+        );
         if (!pMat) {
           this.playerData.knownMaterials.push(entry.name);
-          this.playerData.knownMaterialQuantity.set(entry.name,  entry.count);
+          this.playerData.knownMaterialQuantity.set(entry.name, entry.count);
         } else {
           const pQty = this.playerData.knownMaterialQuantity.get(entry.name);
           this.playerData.knownMaterialQuantity.set(pMat, pQty + entry.count);
@@ -227,5 +260,4 @@ export class TradingComponent {
     this.sellList = [];
     this.buyList = [];
   }
-
 }
